b4c59379b24a7263eecd6aeec675ba7edb44b742
ritika-sharma123 <Ritika.Sharma@coforge.com>
2024-03-11 16:50:55 +0530

    Added:: Scheme migration module

---
+++ src/pages/AfterLogin/SchemaMigration/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Summary from "../../../components/Summary";
+
+const SchemaMigration = () => {
+  return (
+    <>
+      <Summary />
+      <Summary />
+    </>
+  );
+};
+
+export default SchemaMigration;
