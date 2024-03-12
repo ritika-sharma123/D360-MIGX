ff06609886bc9ed2bfaba8cf4da4a9c8c440451f
ritika-sharma123 <Ritika.Sharma@coforge.com>
2024-03-12 13:00:35 +0530

    Updated(core):: changed div to textarea input

--- src/components/Summary/index.js
+++ src/components/Summary/index.js
@@ -1,20 +1,18 @@
 import React from "react";
 import "./index.scss";
+import TextAreaInput from "../Textarea";
 
-const Summary = ({ summary = "SOURCE ORACLE PERSON: DIM_PERSON", detail }) => {
+const Summary = ({
+  summary = "SOURCE ORACLE PERSON: DIM_PERSON",
+  onChange,
+}) => {
   return (
     <>
       <details className="detail-class">
         <summary className="summary-class">{summary}</summary>
         <p>
           <h4>Source SQL</h4>
-          <pre>
-            SELECT column_name(s) FROM table_name WHERE column_name operator ANY
-            (SELECT column_name FROM table_name WHERE condition) SELECT
-            column_name(s) (SELECT column_name FROM table_name WHERE condition);
-            SELECT column_name(s) FROM table_name WHERE column_name operator ANY
-            (SELECT column_name FROM table_name WHERE condition)
-          </pre>
+          <TextAreaInput name={"nnn"} onChange={onChange} />
         </p>
       </details>
     </>
